fa7c63e5d253eec6fee57a498adbdb071fb144c2
Jojo Casidsid <jvcasidsid2013@gmail.com>
2022-02-21 14:27:53 +0800

    add section1 of the module

---
+++ Section-1-Getting-Started/1-what-is-typescript/1-what-is-typescript.tsx
@@ -0,0 +1,17 @@
+// A javascript Superset
+// A language building on javascript
+// Adds new Features + Advantages to Javascript
+
+// Browser cant execute it
+// TS compiled to javascript
+// Feature are compiled to JS workarounrs possible errors are thrown
+
+// Why Typescript
+
+function add(num1: number, num2: number) {
+  return num1 + num2;
+}
+
+console.log(add("2", "3")); //  Unwanted behavior at runtime
+// add if check to add function Validate * Sanitize user input
+// Typescript is a tool that helps developers write better code.
---
+++ Section-1-Getting-Started/3-typescript-overview/index.tsx
@@ -0,0 +1,8 @@
+// Typescript adds
+
+// Types!
+// Non Javascript Features like Interfaces or Generics
+// Next-gen Javascript Features (compiled dow for older Browsers)
+// Meta Programming Features like Decorators
+// Rich configuration options
+// Modern Tolling that helps even in not typescript projects
