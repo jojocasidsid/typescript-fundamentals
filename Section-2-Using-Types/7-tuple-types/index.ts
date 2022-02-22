9d92c48bdc51fe04ebff609474c775b26a7d4f49
Jojo Casidsid <jvcasidsid2013@gmail.com>
2022-02-22 14:53:01 +0800

    update section 2

--- Section-2-Using-Types/7-tuple-types/index.ts
+++ Section-2-Using-Types/7-tuple-types/index.ts
@@ -1,20 +1,18 @@
-// added by typescript ;; Fixed length array
-// you could explicitly assign type object
-// just by assigning an empty type, typescript will have an error
-// you could restrict type to one specific value
-
-const persons = {
+const persons: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string]; // you can explicitly set the type of the tuple
+} = {
   name: "Maximilian",
   age: 30,
   hobbies: ["Sports", "Cooking"],
   role: [2, "author"],
 };
 
-let favoriteActivities: string[]; //means use any type
+let favoriteActivities: string[];
 favoriteActiveites = ["Sports"];
 console.log(persons.name);
-//property nickname does not exist
-//types and assignments are not part of javascript
 
 for (const hobby of persons.hobbies) {
   console.log(hobby);
